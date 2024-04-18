1708346a3a773c38c94713032b07a7b664ab72d3
pabloHuerres01 <p.huerres01@gmail.com>
2024-04-18 18:06:07 +0200

    V2

---
+++ src/main/java/com/ciber15/Calculadora15/Calculadora.java
@@ -0,0 +1,8 @@
+ackage com.ciber15.calculator15;
+import org.springframework.stereotype.Service;
+    @Service
+    public class Calculadora {
+        int sum(int a, int b) {
+        return a + b;
+    }
+}
