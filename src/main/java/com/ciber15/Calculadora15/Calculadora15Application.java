d1ccf89ba638884e706b6d199808d6df17450ca3
pabloHuerres01 <p.huerres01@gmail.com>
2024-04-18 17:59:03 +0200

    V2

---
+++ src/main/java/com/ciber15/Calculadora15/Calculadora15Application.java
@@ -0,0 +1,13 @@
+package com.ciber15.Calculadora15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculadora15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculadora15Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber15/Calculadora15/Calculadora15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber15.Calculadora15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculadora15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
